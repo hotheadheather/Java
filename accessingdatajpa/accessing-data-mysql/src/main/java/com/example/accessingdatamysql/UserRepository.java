423cf80084a76ecbf2a34d66c30847e070a5b6a2
hotheadheather <57191691+hotheadheather@users.noreply.github.com>
2021-02-25 17:22:32 -0600

    Add files via upload

---
+++ accessingdatajpa/accessing-data-mysql/src/main/java/com/example/accessingdatamysql/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.accessingdatamysql;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.accessingdatamysql.User;
+
+//this is an interface that extends crud repository
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
