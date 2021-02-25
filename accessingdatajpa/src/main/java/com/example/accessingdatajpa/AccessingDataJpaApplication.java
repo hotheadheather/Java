f67070a3ea25f5db5831fe53d3a4da7b6f101113
hotheadheather <57191691+hotheadheather@users.noreply.github.com>
2021-02-25 16:50:12 -0600

    Add files via upload

---
+++ accessingdatajpa/src/main/java/com/example/accessingdatajpa/AccessingDataJpaApplication.java
@@ -0,0 +1,64 @@
+package com.example.accessingdatajpa;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Collections;
+
+@SpringBootApplication
+public class AccessingDataJpaApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(AccessingDataJpaApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(AccessingDataJpaApplication.class);
+       // SpringApplication.run(AccessingDataJpaApplication.class);
+        app.setDefaultProperties(Collections
+        .singletonMap("server.port", "8080"));
+        app.run(args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(CustomerRepository repository) {
+        return (args) -> {
+            // save a few customers
+            repository.save(new Customer("Jack", "Bauer"));
+            repository.save(new Customer("Chloe", "O'Brian"));
+            repository.save(new Customer("Kim", "Bauer"));
+            repository.save(new Customer("David", "Palmer"));
+            repository.save(new Customer("Michelle", "Dessler"));
+            repository.save(new Customer("Heather", "Whittlesey"));
+
+            // fetch all customers
+            log.info("Customers found with findAll():");
+            log.info("-------------------------------");
+            for (Customer customer : repository.findAll()) {
+                log.info(customer.toString());
+            }
+            log.info("");
+
+            // fetch an individual customer by ID
+            Customer customer = repository.findById(1L);
+            log.info("Customer found with findById(1L):");
+            log.info("--------------------------------");
+            log.info(customer.toString());
+            log.info("");
+
+            // fetch customers by last name
+            log.info("Customer found with findByLastName('Bauer'):");
+            log.info("--------------------------------------------");
+            repository.findByLastName("Bauer").forEach(bauer -> {
+                log.info(bauer.toString());
+            });
+            // for (Customer bauer : repository.findByLastName("Bauer")) {
+            //  log.info(bauer.toString());
+            // }
+            log.info("");
+        };
+    }
+
+}
